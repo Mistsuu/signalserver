924e925185d3c4c958825f31d5b37a3768813594
Mistsu <vietanhtran12@gmail.com>
2022-07-20 02:39:23 +0700

    Update

---
+++ _test_/client.js
@@ -0,0 +1,12 @@
+const apisauce = require("apisauce");
+
+const api = apisauce.create({
+  headers: {
+    Accept: "application/json",
+    // "Content-Type": "application/x-www-form-urlencoded"
+  },
+  timeout: 30000,
+  baseURL: "http://localhost:1208",
+})
+
+api.post('/todos', { findaway: "toaccessthisjustbyjson" }, { headers: { 'x-ray': 'machine' } })
