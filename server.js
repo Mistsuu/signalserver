8214c8c90b400dc9f595cce7823288360b5c43b9
Mistsu <vietanhtran12@gmail.com>
2022-07-20 02:41:13 +0700

    Update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(1208);
