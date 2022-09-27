4a9522cd4d5bff3aaa9aa624be4f76389057b5a5
Mistsu <vietanhtran12@gmail.com>
2022-09-27 22:26:43 +0700

    Temporary before switch to new branch :)

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const sessions = require('express-session');
 
 // port app use
 const PORT = 1208;
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
 
 // create app
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
-
-
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}));
 
 //username and password
 const myusername = 'user1'
@@ -31,13 +37,13 @@ app.get('/',(req,res) => {
 
 app.post('/user',(req,res) => {
   if(req.body.username == myusername && req.body.password == mypassword){
-      session=req.session;
-      session.userid=req.body.username;
+      session        = req.session;
+      session.userid = req.body.username;
       console.log(req.session)
       res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
   }
   else{
-      res.send('Invalid username or password');
+    res.send('Invalid username or password');
   }
 });
 
