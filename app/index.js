ab1364a97ba7ffd26162ad844ca280bfe82354fa
Mistsu <vietanhtran12@gmail.com>
2022-10-01 23:06:56 +0700

    Update files constructs

--- app/index.js
+++ app/index.js
@@ -5,8 +5,6 @@ const sessions = require('express-session');
 
 // port app use
 const PORT = 1208;
-// creating 24 hours from milliseconds
-const oneDay = 1000 * 60 * 60 * 24;
 
 // create app
 const app = express();
@@ -15,42 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
     resave: false
 }));
-
-//username and password
-const myusername = 'user1'
-const mypassword = 'mypassword'
-
-// a variable to save a session
-var session;
-
-app.get('/',(req,res) => {
-  session = req.session;
-  if (session && session.userid) {
-    res.send("Welcome User <a href=\'/logout'>click to logout</a>");
-  }
-  else
-    res.sendFile('views/index.html',{root:__dirname})
-});
-
-app.post('/user',(req,res) => {
-  if(req.body.username == myusername && req.body.password == mypassword){
-      session        = req.session;
-      session.userid = req.body.username;
-      console.log(req.session)
-      res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
-  }
-  else{
-    res.send('Invalid username or password');
-  }
-});
-
-app.get('/logout',(req,res) => {
-  req.session.destroy();
-  res.redirect('/');
-});
-
-app.listen(PORT, () => {console.log(`Program is running at PORT ${PORT}`);});
---
+++ app/router/index.js
@@ -0,0 +1,9 @@
+const loginRoute    = require("./login.route")
+const registerRoute = require("./register.route")
+const testRoute     = require("./test.route")
+
+module.exports = {
+  loginRoute,
+  registerRoute,
+  testRoute,
+}
