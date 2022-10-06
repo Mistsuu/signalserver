dd5568bceb9d3eb8f71066ebcd69f2bf57360f94
Mistsuuvm <vietanhtran12@gmail.com>
2022-10-06 02:36:13 -0700

    Update string mismatch

--- app/consts/txt.const.js
+++ app/consts/txt.const.js
@@ -4,14 +4,15 @@ const TXT_USERNAME_OR_PASSWORD_IS_WRONG = "Username or password is wrong!";
 const TXT_CANNOT_CREATE_SESSION = "Cannot create session for user!";
 
 // Formats
-const FM_USERNAME_LEN_ERR = "username must be >= {0} and <= {1} in length.";
-const FM_PASSWORD_LEN_ERR = "password must be >= {0} and <= {1} in length.";
+const FM_USERNAME_LEN_ERR = "Username must be >= {0} and <= {1} in length.";
+const FM_PASSWORD_LEN_ERR = "Password must be >= {0} and <= {1} in length.";
 const FM_USER_ALREADY_DEFINED = "User \"{0}\" has already been registered!";
 
 module.exports = {
   // Texts
   TXT_INVALID_REQUEST_FORMAT,
   TXT_USERNAME_OR_PASSWORD_IS_WRONG,
+  TXT_CANNOT_CREATE_SESSION,
   // Formats
   FM_USERNAME_LEN_ERR,
   FM_PASSWORD_LEN_ERR,
