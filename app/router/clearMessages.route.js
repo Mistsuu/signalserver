d986f57363a7a59cee6e5507427b1d75c68e2199
Mistsuuvm <vietanhtran12@gmail.com>
2022-10-11 14:51:18 -0700

    Fix minor code mistake

--- app/router/clearMessages.route.js
+++ app/router/clearMessages.route.js
@@ -5,8 +5,8 @@ module.exports = (req, res) => {
   UserController.clearPendingMessages(req.authData.userID, req.authData.deviceID)
     .then(success => {
       if (success)
-        res.response(ApiConstant.STT_OK).end();
+        res.status(ApiConstant.STT_OK).end();
       else
-        res.response(ApiConstant.STT_INTERNAL_SERVER).end();
+        res.status(ApiConstant.STT_INTERNAL_SERVER).end();
     })
 }
